92b1f52f66bf58506ecb23510d6c874bb4ca9323
lekhika sharma <136784859+Lekhu20@users.noreply.github.com>
2026-06-01 10:47:51 +0530

    Add twoSum function to solve Two Sum problem
    Implemented the twoSum function to find indices of two numbers that add up to a target.

--- Week_1/Day_1/TwoSum.cpp
+++ Week_1/Day_1/TwoSum.cpp
@@ -1,2 +1,18 @@
-# Summer_Assignment_2401920100187
-Projects For summer assignment work 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp ;
+
+        for(int i = 0; i < nums.size(); i++){
+            int avl = target - nums[i];
+            if(mp.find(avl) != mp.end()){
+                return {mp[avl] ,i} ;
+            }
+
+            mp[nums[i]] = i;
+            
+        }
+        return {};
+    }
+};
