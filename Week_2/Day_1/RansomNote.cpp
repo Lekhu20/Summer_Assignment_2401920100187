f4c03529debd5d80bf4aa4199a43629312f9645b
lekhika sharma <136784859+Lekhu20@users.noreply.github.com>
2026-06-08 12:02:52 +0530

    Create RansomNote.cpp

---
+++ Week_2/Day_1/RansomNote.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        vector<int> freq(26,0);
+        for(char ch : s)
+          freq[ch - 'a']++ ;
+          
+        for(int i = 0;i < n; i++){
+            if(freq[s[i]-'a']==1)
+            return i;
+        }
+        return -1;
+    }
+};
