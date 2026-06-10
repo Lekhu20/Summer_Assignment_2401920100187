5d3462fafb197edc19220c139de810542f5b1af6
lekhika sharma <136784859+Lekhu20@users.noreply.github.com>
2026-06-10 12:35:52 +0530

    Create AdultUser.java

---
+++ Week_1/OOPS/AdultUser.java
@@ -0,0 +1,31 @@
+class AdultUser implements LibraryUser{
+   
+    @Override
+    public void registeredAccount() {
+        if(age > 12){
+            System.out.println("You have successfully registered under an Adult Account");
+    }
+        
+        else{
+            System.out.println("Sorry, Age must be greater than 12 to register as an adult");
+        }
+    }
+
+    @Override
+    public void requestBook() {
+        if(booktype.equals("Fiction")){
+            System.out.println("Book Issued successfully, please return the book within 7 days");
+        }
+        else{
+            System.out.println("Oops, you are allowed to take only adult fiction books");
+        }
+    }
+
+    private final int age;
+    private final String booktype;
+    public AdultUser(int age, String bookType){ // setter method
+    this.age=age;
+    this.booktype=bookType;
+    
+}
+}
