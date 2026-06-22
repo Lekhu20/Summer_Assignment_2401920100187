9ca51120a8d8c486d932f7c57159c993280461a8
lekhika sharma <136784859+Lekhu20@users.noreply.github.com>
2026-06-22 10:45:55 +0530

    Create LevelOrderTraversal.cpp

---
+++ Week_4/Day_2/LevelOrderTraversal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root ==NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n= q.size();
+
+            vector<int> l;
+            for(int i = 0; i < n; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+                l.push_back(curr -> val);
+                if(curr -> left)
+                q.push(curr -> left);
+
+                if(curr -> right)
+                q.push(curr -> right);
+            }
+            ans.push_back(l);
+        }
+        return ans;
+    }
+};
