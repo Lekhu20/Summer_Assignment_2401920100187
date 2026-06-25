29319550d2984201f885b83f1ecfbe8584a1ab4e
lekhika sharma <136784859+Lekhu20@users.noreply.github.com>
2026-06-25 09:30:34 +0530

    Create Outer.java

---
+++ Week_4/OOPS_4/Program2_InnerClass/Outer.java
@@ -0,0 +1,13 @@
+class Outer {
+
+    void display() {
+        System.out.println("Display method of Outer Class");
+    }
+
+    class Inner {
+
+        void display() {
+            System.out.println("Display method of Inner Class");
+        }
+    }
+}
