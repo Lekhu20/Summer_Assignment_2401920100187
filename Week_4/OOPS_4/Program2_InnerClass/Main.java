b2c7787062590e90a13ae122bbd337f3a31ef35f
lekhika sharma <136784859+Lekhu20@users.noreply.github.com>
2026-06-25 09:31:19 +0530

    Create Main.java

---
+++ Week_4/OOPS_4/Program2_InnerClass/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outer o = new Outer();
+        o.display();
+
+        Outer.Inner i = o.new Inner();
+        i.display();
+    }
+}
